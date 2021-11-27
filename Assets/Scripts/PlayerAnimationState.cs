1f56a216f1f50658dafaa3fca94b251e4d6ca451
Kian <kianbadiei@gmail.com>
2021-11-27 17:38:42 -0500

    Player Movement And Animation Added

---
+++ Assets/Scripts/PlayerAnimationState.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public enum PlayerAnimationState
+{
+    RUN,
+    IDLE,
+    JUMP,
+    HIT,
+    DEAD,
+    NUM_OF_ANIMATION_STATES
+}
